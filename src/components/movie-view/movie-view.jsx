a2286cd7d6c5ee14100ad30a0f94893bd1be1ebf
schen888 <shuang.chen888@hotmail.com>
2022-10-19 17:43:44 +0200

    Add mz 10 movie datas into the movies array.

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -8,7 +8,7 @@ export class MovieView extends React.Component {
       <div className="movie-view">
 
         <div className="movie-poster">
-          <img src={movie.ImagePath} />
+          <img src={movie.imageURL} />
         </div>
         <div className="movie-title">
           <span className="label">Title: </span>
@@ -17,6 +17,14 @@ export class MovieView extends React.Component {
         <div className="movie-description">
           <span className="label">Description: </span>
           <span className="value">{movie.Description}</span>
+        </div>
+        <div className="movie-genre">
+          <span className="label">Genre: </span>
+          <span className="value">{movie.Genre.Name}</span>
+        </div>
+        <div className="movie-director">
+          <span className="label">Director: </span>
+          <span className="value">{movie.Director.Name}</span>
         </div>
         <button onClick={()=>{ onBackClick(null); }}>Back</button>
 
