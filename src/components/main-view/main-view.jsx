22dddbc262e3cabec4de808623f15635fb5585ad
schen888 <shuang.chen888@hotmail.com>
2022-10-19 17:45:33 +0200

    Move the export sytax before class.

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
 
-class MainView extends React.Component {
+export class MainView extends React.Component {
   constructor(){
     super();
     this.state = {
@@ -54,5 +54,3 @@ class MainView extends React.Component {
     ); */
   }
 }
-
-export {MainView};
