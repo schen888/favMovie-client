9ced795fa486d1b0810bb43c2df0ec444bcf70bf
schen888 <shuang.chen888@hotmail.com>
2022-10-22 17:24:44 +0200

    Add registration-view function component.

---
+++ src/components/registration-view/registration-view.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+
+export function RegistrationView(props) {
+  const [ username, setUsername ] = useState('');
+  const [ password, setPassword ] = useState('');
+  const [ email, setEmail ] = useState('');
+
+  /* const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(username, password);
+
+    props.onLoggedIn(username);
+  }; */
+
+  return (
+    <form>
+      <label>
+        Username:
+        <input type="text" value={username} onChange={e => setUsername(e.target.value)} />
+      </label>
+      <label>
+        Password:
+        <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
+      </label>
+      <label>
+        Email:
+        <input type="email" value={email} onChange={e => setEmail(e.target.value)} />
+      </label>
+      <button type="button">Register</button>
+    </form>
+  );
+}
+
+/* LoginView.propTypes = {
+  onLoggedIn: PropTypes.func.isRequired
+}; */
