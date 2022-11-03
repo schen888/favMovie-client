d4c45a82a683a342eadeca4a51d313062f0fc63d
schen888 <shuang.chen888@hotmail.com>
2022-11-03 15:42:28 +0100

    Export MainView as default

--- src/index.jsx
+++ src/index.jsx
@@ -6,7 +6,7 @@ import { Provider } from 'react-redux';
 import { devToolsEnhancer } from 'redux-devtools-extension';
 import moviesApp from './reducers/reducers';
 
-import  {MainView}  from './components/main-view/main-view';
+import  MainView  from './components/main-view/main-view';
 
 // Import statement to indicate that you need to bundle `./index.scss`
 import './index.scss';
